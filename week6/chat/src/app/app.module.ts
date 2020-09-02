aa3da577bea7d8a340a4889ad686652906bd7ddb
Rebecca <rebecca.lee.barrow.scott@gmail.com>
2020-09-02 21:24:35 +1000

    finish week 6

---
+++ week6/chat/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ChatComponent } from './chat/chat.component';
+import { SocketService } from './services/socket.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ChatComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    CommonModule
+  ],
+  providers: [SocketService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
